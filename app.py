12a799d33183f7f3caf50f43bed9f48ef94e928c
Lexxkit <lexxkit@gmail.com>
2020-06-29 16:22:08 +0300

    flask command for addition initial data to DB

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask_migrate import Migrate
 
 from config import Config
 from models import db
+from csv_to_db import data_cli
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.cli.add_command(data_cli)
 db.init_app(app)
 migrate = Migrate(app, db)
+
 
 from views import *
 
--- csv_to_db.py
+++ csv_to_db.py
@@ -6,10 +6,12 @@ Run this script after flask DB migration and upgrade.
 
 import csv
 import os
+from flask.cli import AppGroup
 
 from models import db, Category, Meal
-from app import app
 from config import current_path
+
+data_cli = AppGroup('add_data')
 
 
 def add_categories():
@@ -30,7 +32,7 @@ def add_meals():
     """Export data from csv items file
         to SQL DB table 'meals'.
         """
-    with open(os.path.join(current_path,'delivery_items.csv'), newline='') as csv_file:
+    with open(os.path.join(current_path, 'delivery_items.csv'), newline='') as csv_file:
         meals = [meal for meal in csv.reader(csv_file)][1:]
 
         for meal in meals:
@@ -45,10 +47,10 @@ def add_meals():
     db.session.commit()
 
 
+@data_cli.command('data')
 def add_data_to_db():
-    with app.app_context():
-        add_categories()
-        add_meals()
+    add_categories()
+    add_meals()
 
 
 if __name__ == '__main__':
